eb719cc2cdbe232312f0023742be6d9a94faa3cb
tetrominos <contact@johansoderblom.se>
2016-03-19 09:51:51 +0100

    updated javascript with parameter

--- makeRandomHexColors.js
+++ makeRandomHexColors.js
@@ -1,46 +1,32 @@
-// Create a hex with # and 6 random hex-numbers.
-function makeColorHex(){
-    var hash = "#";
-    var hexValues = "0123456789ABCDEF";
+/*
+The code create a hex with "#" and 6 random hex-numbers.
+The function takes a String of hexadecimal ("0123456789ABCDEF") 
+as parameter. 
+*/
 
+function makeColorHex(hexValues){
+    var hash = "#";
     for( var i=0; i < 6; i++ ){
 		hash += hexValues.charAt(Math.floor (Math.random() * hexValues.length));
 	}
     return hash;
 }
-
-// Light hexcolor
-function makeColorHexLight(){
-    var hash = "#";
-    var hexValues = "89ABCDEF";
-
-    for( var i=0; i < 6; i++ ){
-		hash += hexValues.charAt(Math.floor (Math.random() * hexValues.length));
-	}
-    return hash;
-}
-
-// Dark hexcolor
-function makeColorHexDark(){
-    var hash = "#";
-    var hexValues = "01234567";
-
-    for( var i=0; i < 6; i++ ){
-		hash += hexValues.charAt(Math.floor (Math.random() * hexValues.length));
-	}
-    return hash;
-}
-
-// Add random hexcolor to background and display teh hexvalue
+/*
+Three example functions that display different
+random hexcolor to background and display the hexvalue.
+*/
 function newBGcolor(){
-    document.body.style.backgroundColor = makeColorHex();
-	document.getElementById("displayHexValue").innerHTML = makeColorHex();
+	var hexValues = "0123456789ABCDEF";
+    document.body.style.backgroundColor = makeColorHex(hexValues);
+	document.getElementById("displayHexValue").innerHTML = makeColorHex(hexValues);
 }
 function newBGcolorLight(){
-    document.body.style.backgroundColor = makeColorHexLight();
-	document.getElementById("displayHexValue").innerHTML = makeColorHexLight();
+	var LIGHThexValues = "89ABCDEF"; //var with Hex that display only bright colors
+    document.body.style.backgroundColor = makeColorHex(LIGHThexValues);
+	document.getElementById("displayHexValue").innerHTML = makeColorHex(LIGHThexValues);
 }
 function newBGcolorDark(){
-    document.body.style.backgroundColor = makeColorHexDark();
-	document.getElementById("displayHexValue").innerHTML = makeColorHexDark();
+	var DARKhexValues = "01234567"; //var with Hex that display only dark colors
+    document.body.style.backgroundColor = makeColorHex(DARKhexValues);
+	document.getElementById("displayHexValue").innerHTML = makeColorHex(DARKhexValues);
 }
